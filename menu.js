ff33d05c80b821aefaa2b1fa8468544cdec36874
Nolan Golz <110066786+nmgolz@users.noreply.github.com>
2022-10-20 15:01:04 -0600

    adding files

---
+++ menu.js
@@ -0,0 +1,164 @@
+class Menu {
+    constructor(name) {
+        this.name = name;
+        this.items =[];
+    }
+
+    createItem(name, cost){
+        this.items.push(new Item(name, cost));
+    }
+};
+
+//this class creates a menu and has a method to add a new item to the Items array
+
+class Item {
+    constructor (name, cost, id){
+        this.name = name;
+        this.cost = cost;
+        this.id = id;
+    }
+}
+//this is the items class that allows for a new menuitem to be created
+
+class menuMaker {
+    static url = 'https://6350167378563c1d82b8a232.mockapi.io/Menu';
+
+    static getMenus(){
+        return $.get(this.url);
+    }
+//gets all menus
+
+    static getMenu(id) {
+        return $.get(this.url + `/${id}`);
+    }
+// gets a specific menu
+
+    static createMenu(menu) {
+        return $.post(this.url, menu);
+    }
+// creates a menu and posts it to the api
+
+    static updateMenu(menu) {
+        return $.ajax({
+            url: this.url +`/${menu.id}`,
+            dataType: 'json',
+            data: JSON.stringify(menu),
+            contentType: 'application/json',
+            type: 'PUT'
+        });
+    }
+// grabs a menus and allows for it to be updated
+
+    static deleteMenu(id) {
+        return $.ajax({
+            url: this.url + `/${id}`,
+            type: 'DELETE'
+        });
+    }
+// deletes a selected menu
+}
+
+// this is the class that houses the link to the api and the methods to return create delete and modify menus. 
+
+//the below class controls the dom and renders or shows the data held in the API
+class DOMManager {
+    static menus;
+
+    static getMenus(){
+        menuMaker.getMenus().then(menus => this.show(menus));
+    }
+
+    static createMenu(name) {
+        menuMaker.createMenu(new Menu(name))
+        .then(() => {
+            return menuMaker.getMenus();
+        })
+        .then((menus) => this.show(menus));
+    }
+
+    static deleteMenu(id){
+        menuMaker.deleteMenu(id)
+        .then(() => {
+            return menuMaker.getMenus();
+        })
+        .then((menus) => this.show(menus));
+    }
+
+    static createItem(id){
+        for(let menu of this.menus) {
+            if(menu.id == id){
+                menu.items.push(new Item($(`#${menu.id}-item-name`).val(), $(`#${menu.id}-item-cost`).val(), Math.floor(Math.random() * 1000)));
+                menuMaker.updateMenu(menu)
+                    .then(() => {
+                        return menuMaker.getMenus();
+                    })
+                    .then((menus) => this.show(menus));
+                }
+            }
+    
+    }
+
+    static removeItem(menuId, itemId) {
+        console.log(menuId, itemId);
+        for(let menu of this.menus){
+            if(menu.id == menuId) {
+                for(let item of menu.items) {
+                    if(item.id == itemId) {
+                        menu.items.splice(menu.items.indexOf(item), 1);
+                        menuMaker.updateMenu(menu)
+                        .then(() => {
+                            return menuMaker.getMenus();
+                        })
+                        .then((menus) => this.show(menus));
+                    }
+                }
+            }
+        }
+    }
+
+    static show(menus) {
+        this.menus = menus;
+        // empties the div with id menu-app then uses a for loop to remake it with the data. 
+        $('#menu-app').empty();
+        for(let menu of menus) {
+            $('#menu-app').append(
+                `<div id="${menu.id}" class="card bg-dark text-dark">
+                    <div class="card-header bg-light">
+                        <h3>${menu.name}</h3>
+                       <div id="menu-items"></div> <br>
+                        <button class="btn btn-dark" onclick="DOMManager.deleteMenu('${menu.id}')">Delete Menu</button>
+                    </div>
+                    <div class="card-body">
+                        <div class="row pb-2">
+                            <div class="col">
+                              <input type="text" id="${menu.id}-item-name" class="form-control" placeholder="New Item">
+                            </div>
+                            <div class="col">
+                                <input type="text" id="${menu.id}-item-cost" class="form-control" placeholder="Cost">
+                            </div>
+                        </div>
+                        <button id="${menu.id}-new-menu-item" onclick="DOMManager.createItem('${menu.id}')" class="btn btn-success">Submit</button>
+                    </div>
+                </div>`
+            );
+            for(let item of menu.items) {
+                $(`#${menu.id}`).find('#menu-items').append(
+                    `<div>
+                        <h5 class="mt-4" id="name-${item.id}"><b>Item:</b> ${item.name}</h5>
+                        <p class="mb-0" id="cost-${item.id}"><b>Cost:</b> $${item.cost}</p>
+                        <button class="btn btn-dark" onclick="DOMManager.removeItem('${menu.id}', '${item.id}')">X</button>
+                    </div>`
+                );
+            }
+        }
+    }
+}
+//this gets the button from the html them uses the method in DOMManager which calls the post method in menuMaker to
+//create and name a new Menu
+$('#create-menu').click(() => {
+    console.log($('#user-input').val());
+    DOMManager.createMenu($('#user-input').val());
+    $('#user-input').val('');
+});
+
+DOMManager.getMenus();
